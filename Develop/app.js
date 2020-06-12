38a4cbda237706089b74a5ffe18821e9e277dae8
Chris Sipe <csipe24@gmail.com>
2020-06-11 22:37:55 -0700

    Finished FS, working application

--- Develop/app.js
+++ Develop/app.js
@@ -11,107 +11,114 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 const employees = [];
+
 const employeeQuestions = [
     {
         type: "input",
         message: "What is the employee name?",
         name: "name"
     },
-
     {
         type: "input",
         message: "What is the employee ID?",
         name: "id"
     },
-
     {
         type: "input",
         message: "What is the employee email?",
         name: "email"
     },
-
-
 ];
-
-inquirer.prompt(questions).then((response)=>{
-console.log(response);
-});
 
 // Ask the user for the position/role
 function askForEmployeeRole(){
+    console.log("----------------");
+    console.log("Adding Team Member");
+    console.log("----------------");
+    inquirer.prompt(
+        {type: "list",
+        message: "What is the employee's role?",
+        name: "role",
+        choices: ["Engineer", "Intern"]})
+            .then ((response) => {   
+                if (response.role === "Engineer"){askforEngineerInfo()}
+                else if ( response.role === "Intern"){askforInternInfo()}
+                });
+};
 
-inquirer.prompt({
-    type: "list",
-    message: "What is the employee's role?",
-    name: "role",
-    choices: ["Engineer", "Intern"]
-                })
-        .then ((response)) =>{   
-            if (response.role === "Engineer"){askforEngineerInfo();}
-            else if ( response.role === "Intern"){askforInternInfo();}
-                            };
-}
-}
+// Ask to add another employee
+function askToAddEmployee(){
+    inquirer.prompt(
+        {type: "list",
+        message: "Would you like to add another employee?",
+        name: "addEmployee",
+        choices: ["Yes", "No"]})
+        .then(({addEmployee}) => {
+            if(addEmployee === "Yes"){askForEmployeeRole();}
+            else{createHTML();
+            }
+        });
+    };
 
 // Ask for Manager Information
-
 function askforManagerInfo(){
+    console.log("Add a new MGR");
+    console.log("----------------");
     inquirer.prompt([...employeeQuestions,
-            {
-                type: "input",
-                message: "What is the employee phone number?",
-                name: "name"
-            },
-        ])
-            .then((response)=> {console.log(response)});
+            {type: "input",
+            message: "What is the employee phone number?",
+            name: "phone"},])
+            .then(({name, id, email, phone}) => {
+                employees.push( new Manager (name, id, email, phone));
+                askForEmployeeRole();
+            })
 };
 
 // Ask for Engineer Information
 function askforEngineerInfo(){
+    console.log("Add a new Engineer");
+    console.log("----------------");
     inquirer.prompt([...employeeQuestions,
-        {
-            type: "input",
-            message: "What is the employee phone number?",
-            name: "name"
-        },
-    ])
-    // Build a new gineer
-        .then((response)=> {console.log(response)});
+        {type: "input",
+        message: "What is the employee's github username?",
+        name: "github"
+        }])
+        .then(({name, id, email, github}) => {
+        // Build a new engineer & Add new engineer to a list
+        employees.push( new Engineer (name, id, email, github));
+        // Ask user to add another employee
+        askToAddEmployee();
+        });
 };
 
 
 function askforInternInfo(){
+    console.log("Add a new Intern");
+    console.log("----------------");
     inquirer.prompt([...employeeQuestions,
-        {
-            type: "input",
-            message: "What is the employee school?",
-            name: "school"
-        },
-    ])
-        .then((response)=> {console.log(response)});
+        {type: "input",
+        message: "What is the intern's school?",
+        name: "school"}])
+        .then(({name, id, email, school}) => {
+            // Build a new intern & add new intern to a list
+            employees.push( new Intern (name, id, email, school));
+            // Ask user to add another employee
+            askToAddEmployee();
+            });
 };
 
+function createHTML() {
+    const html = render(employees);
+    console.log("Team Built");
+    console.log(employees);
+    if(!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
 
-=
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+    fs.writeFile(outputPath, html, (err) =>{
+        if(err){console.log(err);}
+        else { console.log("HTML Created Successfully")}
+    })
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+askforManagerInfo();
